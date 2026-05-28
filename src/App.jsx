5fbb12950c5948d469aa07de1e37b9e55ee47263
Mercs-r <rutikpool@gmail.com>
2026-05-28 13:20:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from "./components/Projects";
 import Certifications from "./components/Certifications";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
